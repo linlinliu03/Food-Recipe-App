7839f5f8b8bd85931fb0e868374e9338f98e6192
linlinliu <linlinliu03@gmail.com>
2020-05-28 09:05:15 -0700

    Fetch data using an API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
 const App = () => {
   const APP_ID = "d7b32178"
   const APP_KEY = "435917edadca3f7b8f382332e69ca819"
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
-  
+  useEffect(() => {
+    getRecipes();
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+  }
+
   return (
      <div className="App">
        <form className="search-form">
