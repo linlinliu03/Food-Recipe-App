a9770edeaaabed8dcd94c1be389a16f20d928d00
linlinliu <linlinliu03@gmail.com>
2020-05-28 08:52:38 -0700

    Add form

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 const App = () => {
   const APP_ID = "d7b32178"
   const APP_KEY = "435917edadca3f7b8f382332e69ca819"
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
   
   return (
      <div className="App">
-       <h1>Hello React</h1>
+       <form className="search-form">
+         <input className="search-bar" type="text"></input>
+         <button className="search-button" type="submit">Search</button>
+       </form>
      </div>
   );
 }
